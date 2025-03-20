3a7dae7c39fcc172060d9063ea26bb050417e0d9
jalvlue <1722292195@qq.com>
2025-03-20 19:27:44 +0800

    assign2: done

--- assign2/main.cpp
+++ assign2/main.cpp
@@ -1,6 +1,7 @@
 /*
  * CS106L Assignment 2: Marriage Pact
- * Created by Haven Whitney with modifications by Fabio Ibanez & Jacob Roberts-Baca.
+ * Created by Haven Whitney with modifications by Fabio Ibanez & Jacob
+ * Roberts-Baca.
  *
  * Welcome to Assignment 2 of CS106L! Please complete each STUDENT TODO
  * in this file. You do not need to modify any other files.
@@ -12,49 +13,83 @@
 #include <queue>
 #include <set>
 #include <string>
-#include <unordered_set>
 
-std::string kYourName = "STUDENT TODO"; // Don't forget to change this!
+std::string kYourName = "S T"; // Don't forget to change this!
 
 /**
- * Takes in a file name and returns a set containing all of the applicant names as a set.
+ * Takes in a file name and returns a set containing all of the applicant names
+ * as a set.
  *
  * @param filename  The name of the file to read.
  *                  Each line of the file will be a single applicant's name.
  * @returns         A set of all applicant names read from the file.
  *
- * @remark Feel free to change the return type of this function (and the function
- * below it) to use a `std::unordered_set` instead. If you do so, make sure
- * to also change the corresponding functions in `utils.h`.
+ * @remark Feel free to change the return type of this function (and the
+ * function below it) to use a `std::unordered_set` instead. If you do so, make
+ * sure to also change the corresponding functions in `utils.h`.
  */
 std::set<std::string> get_applicants(std::string filename) {
-  // STUDENT TODO: Implement this function.
+  std::ifstream file{filename};
+  if (!file.is_open()) {
+    std::cerr << "fail to open file" << filename;
+    std::exit(1);
+  }
+
+  std::string buf;
+  std::set<std::string> ret;
+  while (getline(file, buf)) {
+    ret.insert(buf);
+  }
+  return ret;
+}
+
+auto get_initials(std::string name) {
+  auto pos = name.find(" ");
+  std::string ret;
+  ret.push_back(name[0]);
+  ret.push_back(name[pos + 1]);
+  return ret;
 }
 
 /**
  * Takes in a set of student names by reference and returns a queue of names
  * that match the given student name.
  *
- * @param name      The returned queue of names should have the same initials as this name.
+ * @param name      The returned queue of names should have the same initials as
+ * this name.
  * @param students  The set of student names.
  * @return          A queue containing pointers to each matching name.
  */
-std::queue<const std::string*> find_matches(std::string name, std::set<std::string>& students) {
-  // STUDENT TODO: Implement this function.
+std::queue<const std::string *> find_matches(std::string name,
+                                             std::set<std::string> &students) {
+  std::queue<const std::string *> ret;
+  auto name_initials = get_initials(name);
+
+  for (const auto &n : students) {
+    if (name_initials == get_initials(n)) {
+      ret.push(&n);
+    }
+  }
+
+  return ret;
 }
 
 /**
- * Takes in a queue of pointers to possible matches and determines the one true match!
+ * Takes in a queue of pointers to possible matches and determines the one true
+ * match!
  *
- * You can implement this function however you'd like, but try to do something a bit
- * more complicated than a simple `pop()`.
+ * You can implement this function however you'd like, but try to do something a
+ * bit more complicated than a simple `pop()`.
  *
  * @param matches The queue of possible matches.
  * @return        Your magical one true love.
  *                Will return "NO MATCHES FOUND." if `matches` is empty.
  */
-std::string get_match(std::queue<const std::string*>& matches) {
-  // STUDENT TODO: Implement this function.
+std::string get_match(std::queue<const std::string *> &matches) {
+  if (matches.empty()) {
+    return "NO MATCHES FOUND";
+  }
+  return *matches.front();
 }
 
 /* #### Please don't remove this line! #### */
